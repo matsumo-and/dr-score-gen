9c25f2d87ba536322c92ec6265c5d81b990e5200
matsumo-and <yh134.toisanda@gmail.com>
2025-06-14 02:05:54 +0900

    fix/port

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	r := router.NewRouter()
 
-	log.Println("Server starting on :8081")
-	if err := r.Run(":8081"); err != nil {
+	log.Println("Server starting on :8080")
+	if err := r.Run(":8080"); err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
 }
